580d83df6646e7745e8ed52499f6342057b8eba2
Blackfox <BlackFoxgamingstudio@gmail.com>
2022-09-21 12:58:34 -0700

    working

--- app_config.py
+++ app_config.py
@@ -1,8 +1,10 @@
 import os
 
-CLIENT_ID = "Enter_the_Application_Id_here" # Application (client) ID of app registration
+# Application (client) ID of app registration
+CLIENT_ID = "a93ca8d2-2faa-4a0b-afff-8758d7fc85fd"
 
-CLIENT_SECRET = "Enter_the_Client_Secret_Here" # Placeholder - for use ONLY during testing.
+# Placeholder - for use ONLY during testing.
+CLIENT_SECRET = "cOr8Q~ptsnRjtcUiGudKc9R.DYSWhf_jfMaqvbx7"
 # In a production app, we recommend you use a more secure method of storing your secret,
 # like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
 # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
@@ -10,19 +12,22 @@ CLIENT_SECRET = "Enter_the_Client_Secret_Here" # Placeholder - for use ONLY duri
 # if not CLIENT_SECRET:
 #     raise ValueError("Need to define CLIENT_SECRET environment variable")
 
-AUTHORITY = "https://login.microsoftonline.com/common"  # For multi-tenant app
+AUTHORITY = "https://login.microsoftonline.com/consumers"  # For multi-tenant app
 # AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
-REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
-                              # The absolute URL must match the redirect URI you set
-                              # in the app's registration in the Azure portal.
+# Used for forming an absolute URL to your redirect URI.
+REDIRECT_PATH = "/getAToken45"
+# The absolute URL must match the redirect URI you set
+# in the app's registration in the Azure portal.
 
 # You can find more Microsoft Graph API endpoints from Graph Explorer
 # https://developer.microsoft.com/en-us/graph/graph-explorer
-ENDPOINT = 'https://graph.microsoft.com/v1.0/users'  # This resource requires no admin consent
+# This resource requires no admin consent
+ENDPOINT = 'https://graph.microsoft.com/v1.0/users'
 
 # You can find the proper permission names from this document
 # https://docs.microsoft.com/en-us/graph/permissions-reference
 SCOPE = ["User.ReadBasic.All"]
 
-SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
+# Specifies the token cache should be stored in server-side session
+SESSION_TYPE = "filesystem"
